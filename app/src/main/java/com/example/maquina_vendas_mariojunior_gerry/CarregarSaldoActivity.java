fee729456f5bdfb0896383ce3b1ef11d8337086f
GerryMateusMTrindade <madeiramateus520@gmail.com>
2026-01-30 14:12:57 +0000

    fiz a pagina do signup

--- app/src/main/java/com/example/maquina_vendas_mariojunior_gerry/CarregarSaldoActivity.java
+++ app/src/main/java/com/example/maquina_vendas_mariojunior_gerry/CarregarSaldoActivity.java
@@ -11,25 +11,25 @@ import com.example.maquina_vendas_mariojunior_gerry.models.Utilizador;
 
     public class CarregarSaldoActivity extends AppCompatActivity {
 
-        private Utilizador usuario;
+        private Utilizador user;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_saldo);
 
-            usuario = (Utilizador) getIntent().getSerializableExtra("usuario");
+            user = (Utilizador) getIntent().getSerializableExtra("user");
 
             TextView saldoAtualTextView = findViewById(R.id.saldoAtualTextView);
             EditText valorEditText = findViewById(R.id.valorEditText);
             Button carregarButton = findViewById(R.id.carregarButton);
 
-            saldoAtualTextView.setText("Saldo atual: " + usuario.getSaldo() + " €");
+            saldoAtualTextView.setText("Saldo atual: " + user.getSaldo() + " €");
 
             carregarButton.setOnClickListener(v -> {
                 double valor = Double.parseDouble(valorEditText.getText().toString());
-                usuario.carregarSaldo(valor);
-                saldoAtualTextView.setText("Saldo atual: " + usuario.getSaldo() + " €");
+                user.carregarSaldo(valor);
+                saldoAtualTextView.setText("Saldo atual: " + user.getSaldo() + " €");
                 Toast.makeText(this, "Carregamento efetuado com sucesso!", Toast.LENGTH_SHORT).show();
             });
         }
